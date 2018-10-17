72f20b201a28c8d853822fe419346a94bb8e3307
ThraxHamilton <mmcclenton16@gmail.com>
2018-10-17 00:01:03 -0500

    did some more

--- Program.cs
+++ Program.cs
@@ -4,6 +4,12 @@ using System.Linq;
 
 namespace linqlist {
     class Program {
+        public class Customer
+{
+    public string Name { get; set; }
+    public double Balance { get; set; }
+    public string Bank { get; set; }
+}
         static void Main (string[] args) {
             // Find the words in the collection that start with the letter 'L'
             List<string> fruits = new List<string> () { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
@@ -38,7 +44,7 @@ namespace linqlist {
             IEnumerable<int> fourSixMultiples = numbers.Where (number => number % 4 == 0 || number % 6 == 0);
 
             foreach (int number in fourSixMultiples) {
-                Console.WriteLine (number);
+                // Console.WriteLine (number);
             }
 
             // Order these student names alphabetically, in descending order (Z to A)
@@ -69,9 +75,121 @@ namespace linqlist {
                 "Tre"
             };
 
-            List<string> descend = from nameList in names 
-            orderby w
-            select w;
+            List<string> descend = names.OrderByDescending (name => name).ToList ();
+            foreach (string name in descend) {
+                // Console.WriteLine (name);
+            }
+
+            // Build a collection of these numbers sorted in ascending order
+            List<int> numbers2 = new List<int> () {
+                15,
+                8,
+                21,
+                24,
+                32,
+                13,
+                30,
+                12,
+                7,
+                54,
+                48,
+                4,
+                49,
+                96
+            };
+            IEnumerable<int> numberList = from nums in numbers2
+            orderby nums ascending
+            select nums;
+
+            foreach (int nums in numberList) {
+                // Console.WriteLine(nums);
+            }
+
+            // Output how many numbers are in this list
+            List<int> numbers3 = new List<int> () {
+                15,
+                8,
+                21,
+                24,
+                32,
+                13,
+                30,
+                12,
+                7,
+                54,
+                48,
+                4,
+                49,
+                96
+            };
+
+            // Console.WriteLine(numbers3.Count);
+
+            // How much money have we made?
+            List<double> purchases = new List<double> () {
+                2340.29,
+                745.31,
+                21.76,
+                34.03,
+                4786.45,
+                879.45,
+                9442.85,
+                2454.63,
+                45.65
+            };
+            double purchaseSum = purchases.Sum ();
+            // Console.WriteLine(purchaseSum);
+
+            // What is our most expensive product?
+            List<double> prices = new List<double> () {
+                879.45,
+                9442.85,
+                2454.63,
+                45.65,
+                2340.29,
+                34.03,
+                4786.45,
+                745.31,
+                21.76
+            };
+            double highestPrice = prices.Max ();
+            // Console.WriteLine(highestPrice);
+
+            /*
+    Store each number in the following List until a perfect square
+    is detected.
+
+    Ref: https://msdn.microsoft.com/en-us/library/system.math.sqrt(v=vs.110).aspx
+*/
+            List<int> wheresSquaredo = new List<int> () {
+                66,
+                12,
+                8,
+                27,
+                82,
+                34,
+                7,
+                50,
+                19,
+                46,
+                81,
+                23,
+                30,
+                4,
+                68,
+                14
+            };
+
+            var squared = wheresSquaredo.TakeWhile(num => Math.Sqrt(num) % 1 !=0);
+
+            foreach(int num in squared){
+                Console.WriteLine(num);
+            }
+            
+
+
+            
+
         }
     }
 }
